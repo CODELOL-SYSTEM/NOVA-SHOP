ad6c3056c77112f0cdbd24c14656838bc9c80133
CODELOL-SYSTEM <pc2alex.les@gmail.com>
2026-09-19 22:13:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAZ5vAkAEfIBpflyhxG7ovNdJ67KYKWD0",
+const firebaseConfig = {
+  apiKey: "AIzaSyAZ5vAkAEfIBpfLyhxgO7uvNdJ67KYKWD0",
   authDomain: "novashop-4ee63.firebaseapp.com",
   projectId: "novashop-4ee63",
   storageBucket: "novashop-4ee63.firebasestorage.app",
   messagingSenderId: "1044964015809",
-  appId: "1:1044964015809:web:4eafe48f8539e40",
+  appId: "1:1044964015809:web:4eafe0b1aede48f8539e40",
   measurementId: "G-XNY5X2VMY9"
 };
 
